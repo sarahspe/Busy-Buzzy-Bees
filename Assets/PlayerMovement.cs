6a65b96dc9e121134a7633da8ba560bf0034ec2e
sarahspe <116048109+sarahspe@users.noreply.github.com>
2024-05-29 13:16:09 -0400

    mov

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     void Start()
     {
         
