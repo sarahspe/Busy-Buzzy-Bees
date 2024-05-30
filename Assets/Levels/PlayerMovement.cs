0d1263deca89db5113c144aa9e22654b84941178
sarahspe <116048109+sarahspe@users.noreply.github.com>
2024-05-29 21:36:40 -0400

    oops a lot i forgot to cmmit

--- Assets/Levels/PlayerMovement.cs
+++ Assets/Levels/PlayerMovement.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
-
+    
     void Start()
     {
         
