beab01da41131c98f8a6e0734266a8f9a69f398a
jolene <jolliu26@bergen.org>
2024-06-07 19:59:05 -0400

    MOVING PIPES!!

--- Assets/Scripts/MovePipes.cs
+++ Assets/Scripts/MovePipes.cs
@@ -6,14 +6,10 @@ public class MovePipes : MonoBehaviour
 {
     [SerializeField] private float _speed = 0.65f;
     // Start is called before the first frame update
-    void Start()
-    {
-        transform.position += Vector3.left * _speed * Time.deltaTime;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.left * _speed * Time.deltaTime;
     }
 }
