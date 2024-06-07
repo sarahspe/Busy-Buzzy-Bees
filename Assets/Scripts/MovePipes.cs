418781371ca8793c92a59229f6100fd598dda54d
jolene <jolliu26@bergen.org>
2024-06-07 19:52:39 -0400

    fixed animation

--- Assets/Scripts/MovePipes.cs
+++ Assets/Scripts/MovePipes.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class MovePipes : MonoBehaviour
 {
-    // [SerializeField] private float _speed = 0.65f;
+    [SerializeField] private float _speed = 0.65f;
     // Start is called before the first frame update
     void Start()
     {
-        // transform.position += Vector3.left * _speed * Time.deltaTime;
+        transform.position += Vector3.left * _speed * Time.deltaTime;
     }
 
     // Update is called once per frame
