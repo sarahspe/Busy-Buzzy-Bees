0db5f2951760d2e65f241733f387a62fc87346f7
Vivian Oh <116562546+voh919@users.noreply.github.com>
2024-06-07 00:23:46 -0400

    f

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,9 +15,16 @@ public class NewBehaviorScript : MonoBehaviour
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-        
-        if(Input.GetKey(KeyCode.Space))
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
     }
 
