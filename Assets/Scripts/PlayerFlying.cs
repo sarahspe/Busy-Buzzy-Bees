a1fcf8d2a44b934bf8c5fdd0cec45adcd25602c6
jolene <jolliu26@bergen.org>
2024-06-07 17:38:31 -0400

    added flying

--- Assets/Scripts/PlayerFlying.cs
+++ Assets/Scripts/PlayerFlying.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerFlying : MonoBehaviour
 {
+    [SerializeField] private float _velocity = 1.5f;
+
+    private Rigidbody2D _rb; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+            _rb.velocity = Vector2.up * _velocity;
+
     }
 }
