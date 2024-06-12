f58992073104a3ee1eecbcdf14d3a5d2b347c126
Vivian Oh <116562546+voh919@users.noreply.github.com>
2024-06-11 20:25:04 -0400

    r

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -32,4 +32,16 @@ public class NewBehaviorScript : MonoBehaviour
         anim.SetBool("Walk to Run", horizontalInput != 0);
     }
 
+    //private void Jump()
+    //{
+    //    body.velocity = new Vector2(body.velocity.x, speed);
+    //    grounded = false;
+    //}
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Ground")
+    //        grounded = true;
+    //}
+
 }
