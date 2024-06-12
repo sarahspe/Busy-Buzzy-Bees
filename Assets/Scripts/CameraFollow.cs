d676cae9ee9fa6deb3d3acb3ead33656937836ae
sarahspe <116048109+sarahspe@users.noreply.github.com>
2024-06-12 18:44:46 -0400

    push!

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed = 2f;
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed*Time.deltaTime);
+    }
+}
